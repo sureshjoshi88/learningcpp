511e4b698057c0e05b14f3d1d9423498d7e016e4
sureshjoshi88 <sj3795789@gmail.com>
2025-04-03 12:12:17 +0530

    add new method for input

--- four.cpp
+++ four.cpp
@@ -42,9 +42,45 @@ using namespace std;
 //     cout<<y;
 // }
 
+// int main(){
+//     int x = 10;
+//     double y = 5.55;
+//     double total = x*y;
+//     cout<<"total price = "<<total;
+// }
+
+// int main(){
+//     int x = 10;
+//     int y = 20;
+//     int z = x+y;
+//     cout<<z;
+
+// }
+
+// namespace first{
+//     int num = 10;
+// }
+// int main(){
+//     using namespace first;
+//     cout<<num;
+// }
+
+// int main(){
+//    const double p = 3.14;
+//     double r  = 6;
+//     double total = (p*r*r);
+//     cout<<total;
+// }
+
 int main(){
-    int x = 10;
-    double y = 5.55;
-    double total = x*y;
-    cout<<"total price = "<<total;
+    string name ;
+    cout<<"Enter your name :-";
+    getline(cin>>ws,name);
+    cout<<"entr your job :-";
+    string job;
+    getline(cin>>ws,job);
+    int age;
+    cout<<"enter your age :-";
+    cin>>age;
+    cout<<"my name is "<<name<<" and job is "<<job<<" and age is "<<age;
 }
