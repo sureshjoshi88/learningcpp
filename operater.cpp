e3100e933a158b64bb16be827d2d79925f4eb5ea
sureshjoshi88 <sj3795789@gmail.com>
2025-04-08 10:02:17 +0530

    add new file operater

---
+++ operater.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    // Ternary Operator
+        
+    // Used for small conditions
+    // Condition ? True Output : False Output;
+
+    // int x = 0;
+    // x>=40? cout<<"You pass": cout<<"You fail";
+    
+    // int x = 6;
+    // x%2==0? cout<<"the number is even":cout<<"The number is odd";
+
+    // x>40? cout<<"you are man " : x>10? cout<<"you are boy": x>0?cout<<"You are Kid": cout<<"Enter valid age";
+
+    // all operators
+    // arithmetic operator
+    // + addition
+    // - subtraction
+    // * multiplication
+    // / division
+    // % modulus
+    // ++ increment
+    // -- decrement
+
+    // relational operator
+    // == Equal to
+    // this is != Not Equal to
+    // < Less than
+    // > Greater than
+    // <= Less than or equal to
+    // >= Greater than or equal to
+
+    // Bitwise operator
+    // &  Bitwise AND  both condition true
+    // |  Bitwise OR   either of them be true
+    // ^  Bitwise XOR  accept true when finds opposite conditions
+    // << Left Shift
+    // >> Right Shift
+
+    // Logical operator
+    // && Logical and
+    // || Logical or
+    // ^^  Logical not doesnt exist in C++ but this operator in used in other language like sql rather than this ! is used in c++ 
+
+    // Assignment Operators
+    // = Assignment
+    // += add and assign
+    // -= subtract and assign
+    // *= multiply and assign
+    // /= divide and assign
+    // %= modulus and assign
+    // &= Bitwise AND and assign
+    // |= Bitwise OR and assign
+    // ^= Bitwise XOR and assign
+
+    // Scope Operator
+    // :: scope resolution   class::method
+
+    // Ternary Operator
+    // we?: Condition Expression
+}
