1cc61d03c8cabe996994b2198a28341b61ca3e7c
sureshjoshi88 <sj3795789@gmail.com>
2025-04-10 10:39:23 +0530

    add new array to check even odd number in array

--- Array.cpp
+++ Array.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
+// int main(){
+    // int number[5] = {1,2,3,4,5};
+    // cout<<number[4];
+
+
+
+    // string name[3] = {"suresh","rahul","ankit"};
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     cout<<name[i]<<"\n";
+    // }
+    
+// }
+
 int main(){
-    int number[5] = {1,2,3,4,5};
-    cout<<number[4];
+    int num[6] = {1,2,3,4,5,6};
+    for(int i = 0;i<6;i++){
+        if(num[i]%2==0){
+            cout<<num[i]<<"the number is even"<<"\n";
+        }else{
+            cout<<num[i]<<"the number is odd"<<"\n";
+        }
+    }
 }
