6962e44ac10217d0160c357d365de8692cdf048b
sureshjoshi88 <sj3795789@gmail.com>
2025-04-16 17:32:17 +0530

    to check the 1-100 all number sum

--- loop.cpp
+++ loop.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 
-int main(){
-    for(int i = 1;i<=100;i++){
-        cout<<i<<"\n";
-    }
-}
+// int main(){
+//     for(int i = 1;i<=100;i++){
+//         cout<<i<<"\n";
+//     }
+// }
+
+// int main(){
+//     int sum = 0;
+//     for(int i = 1;i<=100;i++){
+//         sum+=i;
+//     }
+//     cout<<sum;
+// }
