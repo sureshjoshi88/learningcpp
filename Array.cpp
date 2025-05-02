975daa126ee2db586d2b1f17cc19f8753c09f30d
sureshjoshi88 <sj3795789@gmail.com>
2025-05-02 10:35:22 +0530

    to take input and create a 2d array

--- Array.cpp
+++ Array.cpp
@@ -437,28 +437,57 @@ int main(){
 // cout<<sum;
 
 
-    int row =3;
-    int col = 3;
-    int arr[row][col] = {{1,2,3},{4,5,6},{7,8,9}};
-    int arr2[row][col] = {{1,2,3},{4,5,6},{7,8,9}};
-    int arr3[row][col];
+    // int row =3;
+    // int col = 3;
+    // int arr[row][col] = {{1,2,3},{4,5,6},{7,8,9}};
+    // int arr2[row][col] = {{1,2,3},{4,5,6},{7,8,9}};
+    // int arr3[row][col];
 
-    for (int i = 0; i < row; i++)
-    {
-       for (int j = 0; j < col; j++)
-       {
-       arr3[i][j] = arr[i][j]+arr2[i][j];
-       }
+    // for (int i = 0; i < row; i++)
+    // {
+    //    for (int j = 0; j < col; j++)
+    //    {
+    //    arr3[i][j] = arr[i][j]+arr2[i][j];
+    //    }
        
-    }
+    // }
     
-    for (int i = 0; i < row; i++)
-    {
-       for (int j = 0; j < col; j++)
-       {
-        cout<<arr3[i][j]<<" ";
-       }
-       cout<<endl;
+    // for (int i = 0; i < row; i++)
+    // {
+    //    for (int j = 0; j < col; j++)
+    //    {
+    //     cout<<arr3[i][j]<<" ";
+    //    }
+    //    cout<<endl;
+    // }
+    
+
+    int row;
+    int col;
+
+    cout<<"entre a row:-";
+    cin>>row;
+    cout<<"enter a col:-";
+    cin>>col;
+    int arr[row][col];
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++){
+        cout<<"entr a value:-";
+       cin>>arr[i][j];
+      }
+      
+    }
+
+     for (int i = 0; i < row; i++) {
+         cout<<"{";
+      for (int j = 0; j < col; j++){
+        cout<<arr[i][j]<<" ";
+       
+
+      }
+      cout<<"}";
+      cout<<endl;
+      
     }
     
 }
