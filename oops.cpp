dac5fb767fc06524f3962b8951c58a238437ac64
sureshjoshi88 <sj3795789@gmail.com>
2025-05-13 12:29:17 +0530

    ADD NEW CLASS

--- oops.cpp
+++ oops.cpp
@@ -29,29 +29,67 @@ using namespace std;
 // cout<<s2.a;
 // }
 
+// class children{
+//     public:
+//     int a;
+//     int b;
+//     children(int p,int q){
+//        a =p;
+//        b =q;    }
+//     int result(){
+//      cout<<a<<b;
+//     }
+//     children(){
+//         a = 2;
+//     }
+// };
+// int main(){
+//     children c;
+//     children c2(c);
+//     cout<<c2.a;
 
+//     // c.result();
+//     // children.c2(c);
 
-class children{
+// }
+
+// class student{
+//     public:
+//     string name;
+//      int rollno ;
+//      char greade;
+//     student(){
+//         name = "suresh";
+//          rollno = 12345;
+//          greade = 'a';
+//     }
+//     void result(){
+//         cout<<name<<endl;
+//         cout<<rollno<<endl;
+//         cout<<greade<<endl;
+//     }
+// };
+
+// int main(){
+//     student c1;
+//     c1.result();
+// }
+
+class book{
     public:
-    int a;
-    int b;
-    children(int p,int q){
-       a =p;
-       b =q;    }
-    int result(){
-     cout<<a<<b;     
-    }
-    children(){
-        a = 2;
+    string author;
+    string title;
+    int price;
+   
+    void returns(){
+        cout<<author<<"\n"<<title<<"\n"<<price;
     }
 };
+
 int main(){
-    children c;
-    children c2(c);
-    cout<<c2.a;
-
-    // c.result();
-    // children.c2(c);
-
-
+    book b;
+    b.author = "suresh";
+    b.title = "software";
+    b.price = 20000;
+    b.returns();
 }
